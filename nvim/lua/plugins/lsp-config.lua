983c315b85860157e8d8e0c03ce584f569196a32
Juuso Ranne <juuso.ranne@protonmail.com>
2026-01-15 19:52:49 +0200

    updates

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -1,103 +1,139 @@
 -- required plugins for setting up connections to language server
 return {
-  {
-    "williamboman/mason.nvim",
-    opts = {
-      ensure_installed = {
-        "tailwindcss-language-server",
-      }
-    }
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    lazy = false,
-    opts = {
-      ensure_installed = {
-        "bashls", "rust_analyzer", "lua_ls", "marksman", "pyright",
-        "terraformls", "tflint", "jsonls",
-        "gopls", "html", "cssls", "texlab", "ts_ls", "yamlls"
-      },
-      automatic_installation = false,
-    }
-  },
+	{
+		"williamboman/mason.nvim",
+		opts = {
+			ensure_installed = {
+				"tailwindcss-language-server",
+			},
+		},
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		lazy = false,
+		opts = {
+			ensure_installed = {
+				"bashls",
+				"rust_analyzer",
+				"lua_ls",
+				"marksman",
+				"pyright",
+				"terraformls",
+				"tflint",
+				"jsonls",
+				"gopls",
+				"html",
+				"cssls",
+				"texlab",
+				"ts_ls",
+				"yamlls",
+			},
+			automatic_installation = false,
+		},
+	},
 
-  {
-    "neovim/nvim-lspconfig",
-    lazy = false,
-    config = function()
-      local capabilities = vim.lsp.protocol.make_client_capabilities()
-      capabilities.textDocument.foldingRange = {
-        dynamicRegistration = false,
-        lineFoldingOnly = true
-      }
-      capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+	{
+		"neovim/nvim-lspconfig",
+		lazy = false,
+		config = function()
+			local capabilities = vim.lsp.protocol.make_client_capabilities()
+			capabilities.textDocument.foldingRange = {
+				dynamicRegistration = false,
+				lineFoldingOnly = true,
+			}
+			capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
-      local lspconfig = require("lspconfig")
-      local util = require("lspconfig/util")
+			-- Configure LSP servers using vim.lsp.config
+			vim.lsp.config.lua_ls = {
+				capabilities = capabilities,
+			}
 
-      lspconfig.lua_ls.setup({
-        capabilities = capabilities
-      })
-      lspconfig.ts_ls.setup({
-        capabilities = capabilities
-      })
-      lspconfig.bashls.setup({
-        capabilities = capabilities
-      })
-      lspconfig.marksman.setup({
-        capabilities = capabilities
-      })
-      lspconfig.pyright.setup({
-        capabilities = capabilities
-      })
-      lspconfig.terraformls.setup({
-        capabilities = capabilities
-      })
-      lspconfig.tflint.setup({
-        capabilities = capabilities
-      })
-      lspconfig.html.setup({
-        capabilities = capabilities
-      })
-      lspconfig.cssls.setup({
-        capabilities = capabilities
-      })
-      lspconfig.texlab.setup({
-        capabilities = capabilities
-      })
-      lspconfig.gopls.setup({
-        capabilities = capabilities,
-        cmd = { "gopls" },
-        filetypes = { "go", "gomod", "gowork", "gotmpl" },
-        root_dir = util.root_pattern("go.work", "go.mod", ".git"),
-        settings = {
-          gopls = {
-            completeUnimported = true,
-          },
-        },
-      })
-      lspconfig.jsonls.setup({
-        capabilities = capabilities
-      })
-      lspconfig.yamlls.setup({
-        capabilities = capabilities,
-        filetypes = { "yaml", "yml" },
-        root_dir = function(fname)
-          return util.root_pattern(".git")(fname) or vim.fn.getcwd()
-        end,
-        settings = {
-          yaml = {
-            schemas = {
-              ["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*",
-            },
-          },
-        },
-      })
+			vim.lsp.config.ts_ls = {
+				capabilities = capabilities,
+			}
 
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-      vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
-      vim.keymap.set('n', '<leader>r', vim.lsp.buf.rename, { desc = "Rename symbol" })
-    end,
-  },
+			vim.lsp.config.bashls = {
+				capabilities = capabilities,
+			}
+
+			vim.lsp.config.marksman = {
+				capabilities = capabilities,
+			}
+
+			vim.lsp.config.pyright = {
+				capabilities = capabilities,
+			}
+
+			vim.lsp.config.terraformls = {
+				capabilities = capabilities,
+			}
+
+			vim.lsp.config.tflint = {
+				capabilities = capabilities,
+			}
+
+			vim.lsp.config.html = {
+				capabilities = capabilities,
+			}
+
+			vim.lsp.config.cssls = {
+				capabilities = capabilities,
+			}
+
+			vim.lsp.config.texlab = {
+				capabilities = capabilities,
+			}
+
+			vim.lsp.config.gopls = {
+				capabilities = capabilities,
+				cmd = { "gopls" },
+				filetypes = { "go", "gomod", "gowork", "gotmpl" },
+				root_markers = { "go.work", "go.mod", ".git" },
+				settings = {
+					gopls = {
+						completeUnimported = true,
+					},
+				},
+			}
+
+			vim.lsp.config.jsonls = {
+				capabilities = capabilities,
+			}
+
+			vim.lsp.config.yamlls = {
+				capabilities = capabilities,
+				filetypes = { "yaml", "yml" },
+				root_markers = { ".git" },
+				settings = {
+					yaml = {
+						schemas = {
+							["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*",
+						},
+					},
+				},
+			}
+
+			-- Enable all configured LSP servers
+			vim.lsp.enable({
+				"lua_ls",
+				"ts_ls",
+				"bashls",
+				"marksman",
+				"pyright",
+				"terraformls",
+				"tflint",
+				"html",
+				"cssls",
+				"texlab",
+				"gopls",
+				"jsonls",
+				"yamlls",
+			})
+
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+			vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, { desc = "Rename symbol" })
+		end,
+	},
 }
